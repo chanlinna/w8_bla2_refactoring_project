f678e3a367e21559d3ee3d7700d55412c9919345
chanlinna <chanlinnalin@gmail.com>
2026-03-17 22:50:24 +0700

    Start code

---
+++ lib/services/location_service.dart
@@ -0,0 +1,12 @@
+import 'package:w8_bla2_refactoring_project/data/dummy_data.dart';
+
+import '../model/ride/locations.dart';
+
+////
+///   This service handles:
+///   - The list of available rides
+///
+class LocationsService {
+  static const List<Location> availableLocations =
+      fakeLocations; // TODO for now fake data
+}
