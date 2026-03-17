76d13528def69f30dd1a9af8997fce3beceee9bc
chanlinna <chanlinnalin@gmail.com>
2026-03-18 01:11:38 +0700

    BLA-203- implemenrt ride preference state

---
+++ lib/ui/states/ride_preferences_state.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/foundation.dart';
+import 'package:w8_bla2_refactoring_project/data/repositories/ride_preference/ride_preference_repository.dart';
+import '../../model/ride_pref/ride_pref.dart';
+
+class RidePreferenceState extends ChangeNotifier {
+  final RidePreferenceRepository ridePrefRepository;
+
+  RidePreferenceState({required this.ridePrefRepository});
+
+  RidePreference? _selectedPreference;
+  List<RidePreference> _preferenceHistory = [];
+
+  RidePreference? get selectedPreference => _selectedPreference;
+
+  List<RidePreference> get preferenceHistory => _preferenceHistory;
+
+  void loadHistory() {
+    _preferenceHistory = ridePrefRepository.getPreferenceHistory();
+    notifyListeners();
+  }
+
+  void selectPreference(RidePreference preference) {
+    if (preference != _selectedPreference) {
+      _selectedPreference = preference;
+
+      ridePrefRepository.addPreference(preference);
+      _preferenceHistory = ridePrefRepository.getPreferenceHistory();
+
+      notifyListeners();
+    }
+  }
+}
